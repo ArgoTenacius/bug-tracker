0d13352416d317f1c582102a825a6fe110adc781
Ramon Mauricio <ArgoTenacius@gmail.com>
2022-05-12 17:01:21 -0300

    Bug page, new card, integrated with firebase.

--- src/components/bugPage/BugPage.jsx
+++ src/components/bugPage/BugPage.jsx
@@ -3,7 +3,70 @@ import './bugPage.css'
 import { AiOutlinePlus, AiOutlineArrowRight, AiOutlineArrowLeft } from 'react-icons/ai';
 
 const BugPage = ({project}) => {
-  console.log(project);
+  const newBugs = project.Project.bugs.new.bugs;
+  //const wipBugs = project.Project.bugs.wip.bugs;
+  //const doneBugs = project.Project.bugs.done.bugs;
+  const cardNew = (title, data, user, desc = '', id) => (
+    <div key={id} className='bugPage__section-card'>
+      <h1>{title}</h1>
+      <div className='bugPage__section-card--date'>
+        <h6>posted: {data}</h6>
+      </div>
+      <div className='bugPage__section-card--user'>
+        <h5>By: {user}</h5>
+      </div>
+      <p>
+        {desc}
+      </p>
+      <div className='bugPage__section-card--arrow right'>
+        <AiOutlineArrowRight className='bugPage__section-card--arrow---icon'/>
+      </div>
+    </div>
+  )
+
+  const cardWip = (title, data, dataTaken, user, userTaken, desc = '') => (
+    <div className='bugPage__section-card'>
+      <h1>{title}</h1>
+      <div className='bugPage__section-card--date'>
+        <h6>posted: {data}</h6>
+        <h6>taken: {dataTaken}</h6>
+      </div>
+      <div className='bugPage__section-card--user'>
+        <h5>By: {user}</h5>
+        <h5>Taken by: {userTaken}</h5>
+      </div>
+      <p>
+        {desc}
+      </p>
+      <div className='bugPage__section-card--arrow'>
+        <AiOutlineArrowLeft className='bugPage__section-card--arrow---icon'/>
+        <AiOutlineArrowRight className='bugPage__section-card--arrow---icon'/>
+      </div>
+    </div>
+  )
+
+  const cardDone = (title, data, dataTaken, dataFinish, user, userTaken, desc = '') => (
+    <div className='bugPage__section-card'>
+      <h1>{title}</h1>
+      <div className='bugPage__section-card--date'>
+        <h6>posted: {data}</h6>
+        <h6>taken: {dataTaken}</h6>
+        <h6>finished: {dataFinish}</h6>
+      </div>
+      <div className='bugPage__section-card--user'>
+        <h5>By: {user}</h5>
+        <h5>Taken by: {userTaken}</h5>
+      </div>
+      <p>
+        {desc}
+      </p>
+      <div className='bugPage__section-card--arrow'>
+        <AiOutlineArrowLeft className='bugPage__section-card--arrow---icon'/>
+        <AiOutlineArrowRight className='bugPage__section-card--arrow---icon'/>
+      </div>
+    </div>
+  )
+
   return (
     <main className='bugPage'>
       <section className='bugPage__section'>
@@ -11,58 +74,29 @@ const BugPage = ({project}) => {
           <h1>New</h1>
           <AiOutlinePlus className='bugPage__section-header--icon'/>
         </header>
-        <div className='bugPage__section-card'>
-            <h1>Title</h1>
-            <div className='bugPage__section-card--date'>
-              <h6>posted: 06/12/2004</h6>
-            </div>
-            <div className='bugPage__section-card--user'>
-              <h5>By: username</h5>
-            </div>
-          <p>
-            It dont work f
-          </p>
-          <div className='bugPage__section-card--arrow right'>
-            <AiOutlineArrowRight className='bugPage__section-card--arrow---icon'/>
-          </div>
-        </div>
+        {
+          newBugs.map((index, id) => (
+              cardNew(index.title, index.posted, index.email, index.description, id)
+            )
+          )
+        }
       </section>
+
       <section className='bugPage__section'>
         <header className='bugPage__section-header'>
           <h1>Wip</h1>
         </header>
-        <div className='bugPage__section-card'>
-          <header>
-              <h1>Title</h1>
-              <h6>06/12/2004</h6>
-            </header>
-            <h4>By: username</h4>
-            <p>
-              It dont work f
-            </p>
-            <div className='bugPage__section-card--arrow'>
-              <AiOutlineArrowLeft className='bugPage__section-card--arrow---icon'/>
-              <AiOutlineArrowRight className='bugPage__section-card--arrow---icon'/>
-            </div>
-        </div>
+        {
+          cardWip('mouse issue', '06/08/2004', '08/08/2004', 'johndoe@gmail.com', 'janedoe@gmail.com', 'it dont work')
+        }
       </section>
       <section className='bugPage__section'>
         <header className='bugPage__section-header'>
           <h1>Done</h1>
         </header>
-        <div className='bugPage__section-card'>
-          <header>
-            <h1>Title</h1>
-            <h6>06/12/2004</h6>
-          </header>
-          <h4>By: username</h4>
-          <p>
-            It dont work f
-          </p>
-          <div className='bugPage__section-card--arrow'>
-            <AiOutlineArrowLeft className='bugPage__section-card--arrow---icon'/>
-          </div>
-        </div>
+        {
+          cardDone('mouse issue', '06/08/2004', '08/08/2004', '09/08/2004', 'johndoe@gmail.com', 'janedoe@gmail.com', 'it dont work')
+        }
       </section>
     </main>
   )
