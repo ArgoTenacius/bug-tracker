1e341d2a13e942a9d4177bbd3f504f9ffa578a34
Ramon Mauricio <ArgoTenacius@gmail.com>
2022-05-22 10:20:12 -0300

    User page complete.

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,19 +1,26 @@
-import { arrayRemove, collection, doc, setDoc, getDoc } from 'firebase/firestore'
+import { arrayRemove, collection, doc, setDoc, getDoc, arrayUnion } from 'firebase/firestore'
 import React, { useEffect, useState } from 'react'
-import { AiOutlinePlus } from 'react-icons/ai'
+import { AiOutlinePlus, AiFillCheckCircle } from 'react-icons/ai'
+import { MdCancel } from 'react-icons/md'
 import { HiUserRemove } from 'react-icons/hi'
 import { db } from '../../firebase/config'
 import './users.css'
 
 const Users = ({project}) => {
     const [userList, setUserList] = useState([]);
+    const [newUserInput, setNewUserInput] = useState("");
+    const [newUserOpen, setNewUserOpen] = useState(false);
 
     const updateUserList = async () => {
         const docRef = doc(db, 'projects', project.id);
         const docSnap = await getDoc(docRef);
         const data = docSnap.data();
-        
-        setUserList(data.users)
+        setUserList(data.users);
+    }
+    
+    const closeNewUser = () => {
+        setNewUserInput("");
+        setNewUserOpen(false);
     }
 
     useEffect(() => {
@@ -28,6 +35,15 @@ const Users = ({project}) => {
         }, { merge: true })
     }
 
+    const addNewUser = async (user) => {
+        await setDoc(doc(db, 'projects', project.id), {
+            users: arrayUnion(user)
+        }, {merge: true})
+
+        closeNewUser();
+        updateUserList();
+    }
+
     const userCard = (name, key) => (
         <section key={key} className='users__card'>
             <h2>{name}</h2>
@@ -35,13 +51,24 @@ const Users = ({project}) => {
         </section>
     )
 
+    const newUserCard = () => (
+        <div className='users__newCard'>
+            <MdCancel className='users__newCard-icon' onClick={() => closeNewUser()}/>
+            <input className='users__newCard-input' onChange={(e) => setNewUserInput(e.target.value)}/>
+            <AiFillCheckCircle className='users__newCard-icon' onClick={() => addNewUser(newUserInput)}/>
+        </div>
+    )
+
   return (
     <main className='users'>
         <header className='users__header'>
             <h1>Users</h1>
-            <AiOutlinePlus className='users__header-icon'/>
+            <AiOutlinePlus className='users__header-icon' onClick={() => setNewUserOpen(true)}/>
         </header>
         <hr className='users__hr'/>
+        {
+            newUserOpen && newUserCard()
+        }
         {
             userList.map((index, id) => (
                 userCard(index, id)
